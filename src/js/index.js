c2026f444c2c19f685c4f8d797af7759e7bdf485
parisvennis <parisvennis@gmail.com>
2020-08-01 23:01:26 +0000

    initial push

--- src/js/index.js
+++ src/js/index.js
@@ -6,5 +6,19 @@ import "../assets/img/4geeks.ico";
 import "../style/index.scss";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+    var num = ["2", "3", "4", "5", "6", "7 ", "8", "9", "10", "J", "K", "Q", "A"];
+    var suit = ["&#9824", "&#9829", "&#9827", "&#9830"];
+
+    var ranNum = num[Math.floor(Math.random()*num.length)];
+    var ranSuit = suit[Math.floor(Math.random()*suit.length)];
+
+    if (ranSuit == "&#9829" || ranSuit == "&#9830"); 
+    document.querySelector(".card").style.color = "red";
+    
+
+    document.querySelector(".suit1").innerHTML = ranSuit;
+    document.querySelector(".value").innerHTML = ranSuit;
+    document.querySelector(".suit2").innerHTML = ranSuit;
+   
+
 };
